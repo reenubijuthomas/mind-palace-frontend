c18e12e008994ae2768c39cdb046005d6a3ab210
parvathijoshi <parvathijoshi09@gmail.com>
2024-12-06 04:23:26 -0500

    Fixed notification color

--- src/components/Approvals.jsx
+++ src/components/Approvals.jsx
@@ -621,8 +621,9 @@ const Approvals = ({ userId, theme }) => {
       )}
 
       {/* Notification */}
-      {notification && (
-        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-green-500 text-white px-4 py-2 rounded shadow-lg">
+{/* Notification */}
+{notification && (
+        <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 bg-blue-500 text-white px-4 py-2 rounded-md shadow-md">
           {notification}
         </div>
       )}
