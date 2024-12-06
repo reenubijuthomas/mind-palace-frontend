473df19dfc0b81acd2b25d70cdf58953a20f66d9
parvathijoshi <parvathijoshi09@gmail.com>
2024-12-06 04:09:50 -0500

    Moved bin in categories

--- src/components/GroupsPage.jsx
+++ src/components/GroupsPage.jsx
@@ -149,31 +149,32 @@ const GroupsPage = ({ theme }) => {
                 justifyContent: "center",
               }}
             >
-              <button
-                className="absolute top-2 right-2 text-red-500 hover:text-red-700"
-                onClick={(e) => {
-                  e.stopPropagation(); 
-                  setCategoryToDelete(category.id); 
-                  setIsModalOpen(true); 
-                }}
-                title="Delete this category"
-              >
-                <svg
-                  aria-hidden="true"
-                  focusable="false"
-                  data-prefix="fas"
-                  data-icon="trash"
-                  className="w-6 h-6"
-                  role="img"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 448 512"
-                >
-                  <path
-                    fill="currentColor"
-                    d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
-                  ></path>
-                </svg>
-              </button>
+             <button
+  className="absolute bottom-2 right-2 text-red-500 hover:text-red-700"
+  onClick={(e) => {
+    e.stopPropagation();
+    setCategoryToDelete(category.id);
+    setIsModalOpen(true);
+  }}
+  title="Delete this category"
+>
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    data-prefix="fas"
+    data-icon="trash"
+    className="w-6 h-6"
+    role="img"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 448 512"
+  >
+    <path
+      fill="currentColor"
+      d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"
+    ></path>
+  </svg>
+</button>
+
               <h3 className="text-lg font-bold">{category.name}</h3>
             </div>
           ))
